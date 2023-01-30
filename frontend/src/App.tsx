e4bdb7670a7a145a831425c1b358e3a61d8c59b5
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-30 09:32:41 -0300

    Static charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import Navbar from "components/Navbar";
+import Footer from "components/Footer";
+import DataTable from "components/DataTable";
+import Barchart from "components/BarChart";
+import Donutchart from "components/DonutChart";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Navbar/>
+    <div className="container">
+      <h1 className="text-primary py-3">Bashboard de Vendas</h1>
+      <div className="row px-3">
+          <div className="col-sm-6">
+          <h5 className="text-center text-secondary">Total de Venda</h5>
+            <Barchart/>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Total de Venda</h5>
+            <Donutchart/>
+          </div>
+      </div>
+      <div className="py-3">
+        <h2 className="text-primary">Todas Vendas</h2>
+      </div>
+      <DataTable/>
     </div>
+    <Footer/>
+    </>
   );
 }
 
